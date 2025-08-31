53662f6c96173868ff1190a21b4a622acf0d7802
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-08-31 08:32:06 +0000

    search & news component

---
+++ client/src/components/News.jsx
@@ -0,0 +1,7 @@
+export default function News() {
+  return (
+    <div className="flex flex-col p-3 justify-between h-screen items-center">
+      News
+    </div>
+  );
+}
--- client/src/components/RightSideBar.jsx
+++ client/src/components/RightSideBar.jsx
@@ -1,3 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import News from "./News";
+
 export default function RightSideBar() {
-  return <div className="text-slate-500"> RightSideBar</div>;
+  const [input, setInput] = useState("");
+  const router = useRouter();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!input.trim()) return;
+    router.push(`/search/${input}`);
+  };
+  return (
+    <>
+      <div className="sticky top-0 bg-white py-2">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 "
+          ></input>
+        </form>
+        <News/>
+      </div>
+    </>
+  );
 }
