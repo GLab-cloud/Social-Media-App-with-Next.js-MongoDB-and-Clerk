bf633bc7ec3bd8452f226975fa6a80e4d4f13967
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-09-06 07:25:56 +0000

    createOrUpdateUser

---
+++ client/src/lib/mongodb/actions/user.js
@@ -0,0 +1,30 @@
+import User from "../models/user.model";
+import { connect } from "mongoose";
+export const createOrUpdateUser = async (
+  id,
+  first_name,
+  last_name,
+  image_url,
+  email_addresses,
+  username
+) => {
+  await connect();
+  try {
+    const user = await User.findOneAndUpdate(
+      { clerkId: id },
+      {
+        $set: {
+          firstName: first_name,
+          lastName: last_name,
+          avatar: image_url,
+          email: email_addresses[0].email_address,
+          username,
+        },
+      },
+      { new: true, upsert: true }
+    );
+    return user;
+  } catch (error) {
+    console.log("Error creating or update user", error);
+  }
+};
