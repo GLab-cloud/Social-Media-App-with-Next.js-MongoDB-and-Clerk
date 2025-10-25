f32118670039a95d889fbe3a97861dd554295749
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-10-25 12:21:28 +0000

    cache: "no-store"

--- next-app/src/app/page.js
+++ next-app/src/app/page.js
@@ -3,16 +3,16 @@ import Feed from "@/components/Feed";
 
 export default async function Home() {
   let data = null;
-  try {
+  
     const result = await fetch(process.env.URL + "/api/post/all", {
       method: "POST",
       cache: "no-store",
     }); // fetch by server side - process.env.URL+...
     data = await result.json();
     console.log("posts data: ", data);
-  } catch (error) {
-    console.log("Error fetching posts", error);
-  }
+   if (!data) {
+    console.log("Error fetching posts");
+
   return (
     <div className="h-screen max-w-xl mx-auto w-full">
       {" "}
