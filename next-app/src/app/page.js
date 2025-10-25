a9b801d11c112faf5abd2257250c4a1e20fa1f3b
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-10-25 12:11:31 +0000

    Dynamic server usage: no-store fetch

--- next-app/src/app/page.js
+++ next-app/src/app/page.js
@@ -6,7 +6,7 @@ export default async function Home() {
   try {
     const result = await fetch(process.env.URL + "/api/post/all", {
       method: "POST",
-      cache: "no-store",
+      // cache: "no-store",
     }); // fetch by server side - process.env.URL+...
     data = await result.json();
     console.log("posts data: ", data);
