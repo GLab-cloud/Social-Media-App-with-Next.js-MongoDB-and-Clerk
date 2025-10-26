e7fbe54a5dbfa64a4fbb241c8eeab163ed892fbb
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-10-26 03:52:57 +0000

    likePost func

--- next-app/src/components/Icons.jsx
+++ next-app/src/components/Icons.jsx
@@ -1,12 +1,56 @@
 "use client";
 
-import { HiOutlineChat, HiOutlineHeart, HiOutlineTrash } from "react-icons/hi";
+import { useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import {
+  HiOutlineChat,
+  HiOutlineHeart,
+  HiOutlineTrash,
+  HiHeart,
+} from "react-icons/hi";
 
-export default function Icons() {
+export default function Icons({ post }) {
+  const [isLiked, setisLiked] = useState(false);
+  const [likes, setLikes] = useState(post.likes || []);
+  const { user } = useUser();
+  const router = useRouter();
+  const likePost = async () => {
+    if (!user) {
+      router.push("/sign-in");
+      return;
+    }
+    const like = await fetch("/api/post/like", {
+      method: PUT,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ postId: post._id }),
+    });
+  };
+  if (isLiked) {
+    setLikes(likes.filter((like) => like !== user.publicMetadata.userMongoId));
+  }
+
   return (
     <div className="flex justify-start gap-5 p-2 text-gray-500">
       <HiOutlineChat className="h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100" />
-      <HiOutlineHeart className="h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100" />
+      {isLiked ? (
+        <HiHeart
+          onClick={likePost}
+          className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 text-red-600 hover:text-red-500 hover:bg-red-100 "
+        />
+      ) : (
+        <HiOutlineHeart
+          onClick={likePost}
+          className="h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
+        />
+      )}{" "}
+      {likes.length > 0 && (
+        <span className={`text-xs ${isLiked && "text-red-600"}`}>
+          {likes.length}
+        </span>
+      )}
       <HiOutlineTrash className="h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100" />
     </div>
   );
