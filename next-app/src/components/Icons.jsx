a2210bc8606e50423290fc4c397aa2856f89529b
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-10-29 07:51:06 +0000

    likePost api func

--- next-app/src/components/Icons.jsx
+++ next-app/src/components/Icons.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import {
@@ -15,42 +15,57 @@ export default function Icons({ post }) {
   const [likes, setLikes] = useState(post.likes || []);
   const { user } = useUser();
   const router = useRouter();
-  const likePost = async () => {
+  const likePost = () => {
     if (!user) {
       router.push("/sign-in");
       return;
     }
-    const like = await fetch("/api/post/like", {
-      method: PUT,
+    const like = fetch("/api/post/like", {
+      method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ postId: post._id }),
     });
+    if (like && isLiked) {
+      setLikes(
+        likes.filter((like) => like !== user.publicMetadata.userMongoId)
+      );
+    }
+    if (like && !isLiked) {
+      setLikes([...likes, user.publicMetadata.userMongoId]);
+    }
+    console.log("likes", likes, like);
   };
-  if (isLiked) {
-    setLikes(likes.filter((like) => like !== user.publicMetadata.userMongoId));
-  }
+  useEffect(() => {
+    if (user && likes?.includes(user.publicMetadata.userMongoId)) {
+      setisLiked(true);
+    } else {
+      setisLiked(false);
+    }
+  }, [likes, user]);
 
   return (
     <div className="flex justify-start gap-5 p-2 text-gray-500">
       <HiOutlineChat className="h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100" />
-      {isLiked ? (
-        <HiHeart
-          onClick={likePost}
-          className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 text-red-600 hover:text-red-500 hover:bg-red-100 "
-        />
-      ) : (
-        <HiOutlineHeart
-          onClick={likePost}
-          className="h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
-        />
-      )}{" "}
-      {likes.length > 0 && (
-        <span className={`text-xs ${isLiked && "text-red-600"}`}>
-          {likes.length}
-        </span>
-      )}
+      <div className="flex items-center">
+        {isLiked ? (
+          <HiHeart
+            onClick={likePost}
+            className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 text-red-600 hover:text-red-500 hover:bg-red-100 "
+          />
+        ) : (
+          <HiOutlineHeart
+            onClick={likePost}
+            className="h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
+          />
+        )}{" "}
+        {likes.length > 0 && (
+          <span className={`text-xs ${isLiked && "text-red-600"}`}>
+            {likes.length}
+          </span>
+        )}
+      </div>
       <HiOutlineTrash className="h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100" />
     </div>
   );
